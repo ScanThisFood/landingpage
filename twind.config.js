430065d824f70fa31c6d4223a92d8aa06269613f
Luca Gerlich <lucagerlich@outlook.de>
2022-11-27 17:22:47 +0100

    Init configs
    twind, prettier, eslint

---
+++ twind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('twind').Configuration} */
+const config = {
+  theme: {
+    extend: {
+      screens: {
+        standalone: { raw: "(display-mode:standalone)" },
+      },
+      colors: {
+        slate_blue: "#0C121C",
+        green: "#005A2B",
+        white: "#FFFFFF",
+        light_cool_grey: "#F8F9F9",
+        cool_light_grey: "#E4E8E8",
+      },
+    },
+  },
+};
+
+export default config;
